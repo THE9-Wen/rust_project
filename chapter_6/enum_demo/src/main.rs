84b4e9a8929ae4ff77ee9cb95b34760b1f312030
Wenhao Tong <854772137@qq.com>
2023-04-18 23:47:39 +0800

    chapter 6

---
+++ chapter_6/enum_demo/src/main.rs
@@ -0,0 +1,67 @@
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let x = 3;
+    let y: Option<i32> = Some(6);
+
+    let y = plus_one(y);
+
+    match x {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        _ => (),
+    };
+
+    if let 5 = x {
+        println!("five");
+    } else {
+        println!("not five");
+    }
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> i32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
