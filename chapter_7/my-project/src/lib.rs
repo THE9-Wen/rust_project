763bd6ad130c3638557ee9c2d027203ae0bfbba7
Wenhao Tong <854772137@qq.com>
2023-04-19 23:12:59 +0800

    chapter 7

--- chapter_7/my-project/src/lib.rs
+++ chapter_7/my-project/src/lib.rs
@@ -1,10 +1,7 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-}
+// use crate::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
 
 mod serving {
     fn take_order() {}
@@ -20,4 +17,38 @@ pub fn eat_at_restaurant() {
 
     // relative
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+
+    // meal.season_fruit = String::from("Watermelon");
+
+    let soup = back_of_house::Appetizer::Soup;
+    let salad = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        season_fruit: String,
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                season_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
