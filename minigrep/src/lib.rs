c78f410db1afd1dc7f6328ea8c8bf41d32ee7a89
Wenhao Tong <854772137@qq.com>
2023-04-23 02:51:23 +0800

    chapter 12

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,14 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let string = fs::read_to_string(config.file_path)?;
-    for line in search(&config.query, &string) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &string)
+    } else {
+        search(&config.query, &string)
+    };
+    for line in results.iter() {
         println!("{line}");
     }
     Ok(())
@@ -12,6 +17,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -23,9 +29,12 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         return Ok(Config{
             query,
             file_path,
+            ignore_case,
         })
     }
 }
@@ -40,17 +49,38 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
     result
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn test_search() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 pick three";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "DucT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three";
+        assert_eq!(vec!["safe, fast, productive."], search_case_insensitive(query, contents));
     }
 }
