f4e71ee37376573c872cfa5cf634ba92c56c7be6
Wenhao Tong <854772137@qq.com>
2023-04-23 00:41:11 +0800

    chapter 12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, process};
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    };
 }
